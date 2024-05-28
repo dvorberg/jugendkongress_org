eb8b4af6dcd21f2fac4d3fce81c02d8205213f00
Diedrich Vorberg <diedrich@tux4web.de>
2024-05-28 12:54:40 +0200

    New SECRET

--- etc/config-development.py
+++ etc/config-development.py
@@ -2,9 +2,8 @@ import socket
 
 hostname = socket.gethostname().split(".")[0]
 
-SECRET_KEY="eDc-4Zm-AU2-v07"
+SECRET_KEY="2FK-wi8-HLT-nhk"
 SITE_URL=f"http://jugendkongress.{hostname}.tux4web.de"
 WWW_PATH="/Users/diedrich/jugendkongress/www"
 SKIN_PATH="/Users/diedrich/jugendkongress/skin"
-BRAND="jugendkongress"
-DATASOURCE={ "dbname": BRAND, }
+DATASOURCE={ "dbname": "jugendkongress", }
