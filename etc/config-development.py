b339f61f5d9cd8813214322218305577ef00391c
Diedrich Vorberg <diedrich@tux4web.de>
2024-05-28 19:25:02 +0200

    Changed development URL

--- etc/config-development.py
+++ etc/config-development.py
@@ -3,7 +3,7 @@ import socket
 hostname = socket.gethostname().split(".")[0]
 
 SECRET_KEY="2FK-wi8-HLT-nhk"
-SITE_URL=f"http://jugendkongress.{hostname}.tux4web.de"
+SITE_URL=f"http://juko.{hostname}.tux4web.de"
 WWW_PATH="/Users/diedrich/jugendkongress/www"
 SKIN_PATH="/Users/diedrich/jugendkongress/skin"
 DATASOURCE={ "dbname": "jugendkongress", }
