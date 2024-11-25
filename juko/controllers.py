b838934a3630bf4e6968cf3f6578d1062c19d944
Diedrich Vorberg <diedrich@tux4web.de>
2024-11-25 16:10:44 +0100

    Workshop selection and booking deletion

--- juko/controllers.py
+++ juko/controllers.py
@@ -19,17 +19,19 @@
 ##  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ##
 ##  I have added a copy of the GPL in the file LICENSE
-
+import re
 import flask
-from .utils import gets_parameters_from_request
+from .utils import (gets_parameters_from_request, process_template,
+                    rget, redirect)
 
 from t4.res import email_re
 from t4.passwords import random_password
+from t4.sendmail import sendmail
 
 from sqlclasses import sql
 
 from .db import execute, query_one, insert_from_dict, commit
-from .model.congress import Booking
+from .model.congress import Booking, Change, Congress
 
 @gets_parameters_from_request
 def create_booking(congress, email="", firstname=None, lastname=None,
@@ -41,7 +43,7 @@ def create_booking(congress, email="", firstname=None, lastname=None,
         errors["email"] = "Bitte gib eine gültige e-Mail Adresse ein."
     else:
         count, = query_one(f"SELECT COUNT(*) FROM booking "
-                           f" WHERE congress_year = {congress.year} "
+                           f" WHERE year = {congress.year} "
                            f"   AND email = %s", (email,))
         if count != 0:
             errors["email"] = ("Es gibt schon eine Buchung mit dieser "
@@ -50,7 +52,9 @@ def create_booking(congress, email="", firstname=None, lastname=None,
                     "errors": errors}
 
     if checkmail:
-        return { "errors": { "email": None }}
+        if not "email" in errors:
+            errors["email"] = None
+        return { "errors": errors}
 
     if firstname is not None:
         firstname = firstname.strip()
@@ -69,18 +73,19 @@ def create_booking(congress, email="", firstname=None, lastname=None,
         while count != 0:
             slug = random_password(9, use_specials=False)
             count, = query_one(f"SELECT COUNT(*) FROM booking "
-                               f" WHERE congress_year = {congress.year} "
+                               f" WHERE year = {congress.year} "
                                f"   AND slug = %s", (slug,))
 
-        booking = { "congress_year": congress.year,
+        booking = { "year": congress.year,
                     "email": email,
                     "slug": slug,
                     "firstname": firstname,
-                    "lastname": lastname, }
+                    "lastname": lastname,
+                    "user_agent": flask.request.headers.get("User-Agent")}
         insert_from_dict("booking", booking, retrieve_id=False)
         commit()
 
-        send_booking_email(Booking.from_dict(booking))
+        send_booking_email(congress, Booking.from_dict(booking))
 
         return { "errors": errors,
                  "created": True,
@@ -88,17 +93,73 @@ def create_booking(congress, email="", firstname=None, lastname=None,
                 }
 
 @gets_parameters_from_request
-def resend_welcome(congress, email=None):
+def resend_booking_email(congress, email=None):
     booking = Booking.select_one(
         congress.where("email = ", sql.string_literal(email)))
-    send_booking_email(booking)
+    send_booking_email(congress, booking)
 
     return { "resent": True,
              "errors": {} }
 
 
-def send_booking_email(booking:Booking):
-    pass
+def send_booking_email(congress:Congress, booking:Booking):
+    template_path = flask.g.skin.resource_path(
+        "skin/jugendkongress/booking_email.txt")
+    with open(template_path) as fp:
+        template = fp.read()
 
-def modify_booking(congress):
-    return {}
+    text = process_template(template, congress=congress, booking=booking)
+
+    subject = (f"Anmeldung zum {congress.nummer} Lutherischen Jugendkongress "
+               f"{congress.year}")
+    sendmail("Jugendkongress Anmeldungs-Roboter",
+             "anmeldung@jugendkongress.org",
+             booking.name,
+             booking.email,
+             subject,
+             text)
+
+
+def modify_booking(congress, slug):
+    change = Change(flask.request.form)
+    Booking.validate(change)
+
+    values = change.validated
+    values["user_agent"] = flask.request.headers.get("User-Agent")
+    if (values):
+        Booking.update_by_slug(congress.year, slug, **values)
+        commit()
+
+    return { "errors": change.errors }
+
+def delete_booking(congress, slug):
+    execute("DELETE FROM booking WHERE year = %s AND slug = %s",
+            (congress.year, slug, ))
+    commit()
+
+    return redirect(congress.href,
+                    site_message=("Deine Buchung wurde storniert. "
+                                  "Am besten, du löscht auch die "
+                                  "Mail, denn der Link funkioniert "
+                                  "jetzt nicht mehr."),
+                    site_message_class="info")
+
+@gets_parameters_from_request
+def select_workshop(congress, slug, workshop_id):
+    execute("INSERT INTO workshop_choices "
+            "( SELECT id, %s FROM booking WHERE year = %s AND slug = %s)"
+            "    ON CONFLICT DO NOTHING",
+            ( workshop_id, congress.year, slug))
+    commit()
+    return { "selected": workshop_id }
+
+@gets_parameters_from_request
+def drop_workshop(congress, slug, workshop_id):
+    execute("DELETE FROM workshop_choices "
+            " WHERE workshop_id = %s"
+            "   AND booking_id = (SELECT id FROM booking "
+            "                      WHERE year = %s AND slug = %s)",
+            (workshop_id, congress.year, slug))
+    commit()
+
+    return { "selected": [] }
