e1d85d0904caa0a3a9fc12e7c0f7d0f048784273
Diedrich Vorberg <diedrich@tux4web.de>
2025-01-22 22:17:33 +0100

    Moved UIBlocker to its own js file

--- www/skin/jugendkongress/booking.js
+++ www/skin/jugendkongress/booking.js
@@ -1,70 +1,3 @@
-class UIBlocker
-{
-	constructor()
-	{
-		const self = this;
-		this.blocked = false;
-
-		Object.keys(window).forEach(key => {
-			if (/^on(key|mouse)/.test(key)) {
-				window.addEventListener(
-					key.slice(2), this.on_event.bind(this));
-			}
-		});
-
-		this.spinner = null;
-		this.main_button = null;
-		window.addEventListener("load", function(event) {
-			self.spinner = document.querySelector("img#spinner");
-			if (self.spinner) self.spinner.style.visibility = "hidden";
-			self.main_button = document.querySelector("button.main");
-			if (self.main_button) self.main_button.classList.remove("disabled");
-		});
-	}
-	
-	block()
-	{
-		if (this.blocked)
-		{
-			return;
-		}
-		else
-		{
-			this.blocked = true;
-			document.body.classList.add("wait");
-		}
-		
-		if (this.spinner) this.spinner.style.visibility = "visible";
-		if (self.main_button) self.main_button.classList.add("disasbled");
-	}
-
-	unblock()
-	{
-		if (! this.blocked)
-		{
-			return;
-		}
-		else
-		{
-			this.blocked = false;
-			document.body.classList.remove("wait");
-		}
-		
-		if (this.spinner) this.spinner.style.visibility = "hidden";
-		if (self.main_button) self.main_button.classList.remove("disabled");
-	}
-
-	on_event(event)
-	{
-		if (this.blocked)
-		{
-			event.preventDefault();			
-		}
-	}
-}
-
-const uiblocker = new UIBlocker();
-
 class BookingController
 {
 	constructor(div)
@@ -416,8 +349,13 @@ class BookingFormController extends BookingController
 				input.addEventListener("keyup", event => {
 					self.dirty = true;
 					
+					self.div.classList.remove("was-validated");
+					input.classList.remove("is-valid");
+					input.classList.remove("is-invalid");
+					
 					if (event.keyCode == 13)
 					{
+						console.log(input);
 						self.send_from_input(input);
 					}
 				});
@@ -458,6 +396,8 @@ class BookingFormController extends BookingController
 			});
 			input.addEventListener("keyup", event => {
 				self.dirty = true;
+				input.classList.remove("is-valid");
+				input.classList.remove("is-invalid");
 			});
 			input.value = booking[input.id];
 		});			
