2215c11f289c1a40b7e8200e0229b40e65436e48
Diedrich Vorberg <diedrich@tux4web.de>
2025-11-19 10:30:48 +0100

    2026 has forced dark mode.

--- www/2026_come_together/custom_2026.js
+++ www/2026_come_together/custom_2026.js
@@ -1,9 +1,3 @@
 window.addEventListener("load", function(event) {
-	function set_dark_scheme()
-	{
-		document.documentElement.setAttribute("data-bs-theme", "dark");
-	}
-	
-	window.matchMedia('(prefers-color-scheme: dark)').addEventListener(
-		"change", set_dark_scheme);
+	document.documentElement.setAttribute("data-bs-theme", "dark");
 });
